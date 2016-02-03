50e980be3404b7be6267608b882d613736ef9d10
Zied Brini <briniz@tcd.ie>
2016-02-03 20:07:57 +0000

    Modify the settings to make them work on PyCharm.

--- rheia_proj/rheia_settings.py
+++ rheia_proj/rheia_settings.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse_lazy
 
-from rheia_proj.settings import INSTALLED_APPS, SECRET_KEY
+from rheia_proj.settings import INSTALLED_APPS, SECRET_KEY, STATIC_URL, ROOT_URLCONF
 
 INSTALLED_APPS += ("rheia", "bootstrap3")
 LOGIN_REDIRECT_URL = reverse_lazy("whoami")
+DEBUG = True
