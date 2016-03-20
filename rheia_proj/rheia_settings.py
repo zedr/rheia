a190d34b9c170e1a7f199200e9db9bcb142a42d5
Rigel Di Scala <rigel.discala@propylon.com>
2016-03-20 14:46:07 +0000

    Add support for the SVG and SVGZ mimetypes.

--- rheia_proj/rheia_settings.py
+++ rheia_proj/rheia_settings.py
@@ -3,6 +3,10 @@ from django.core.urlresolvers import reverse_lazy
 from rheia_proj.settings import (
     INSTALLED_APPS, SECRET_KEY, STATIC_URL, ROOT_URLCONF
 )
+import mimetypes
+
+mimetypes.add_type("image/svg+xml", ".svg", True)
+mimetypes.add_type("image/svg+xml", ".svgz", True)
 
 INSTALLED_APPS += (
     "rheia",
